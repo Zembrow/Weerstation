ebcfccb5c59781e625447226fc8f640eb7fed983
Freark de Lange <Frearkdelange2002@hotmail.com>
2023-11-24 20:22:07 +0100

    Read BME680-sensor
    - Correctly read BME680-sensor data;
    - Added all required & relevant libraries, variables & functions.

--- Weerstation.ino
+++ Weerstation.ino
@@ -1,9 +1,52 @@
-void setup() {
-  // put your setup code here, to run once:
+// Include libraries
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME680.h>
 
+// Define components
+Adafruit_BME680 bme; // BME680-Sensor
+
+// Global variables
+//
+// BME680-sensor readings
+float insideTemperature;
+float insideHumidity;
+float insidePressure;
+float insideGas;
+
+// BME680-sensor units
+String insideTemperatureUnit = "*C";
+String insideHumidityUnit = "%";
+String insidePressureUnit = "hPa";
+String insideGasUnit = "KOhms";
+
+// Setup-function
+// Function runs once when ESP32 is booted
+void setup() {
+  // [FOR DEBUGGING ONLY] Open serial channel
+  Serial.begin(115200);
+
+  // Init BME680-sensor
+  bme.begin();
 }
 
+// Loop-function
+// Function loops continuously 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Read BME680-sensor
+  bme.beginReading();
+  delay(50);
+  bme.endReading();
 
+  // Read BME680 sensor-data from reading
+  insideTemperature = bme.temperature;
+  insideHumidity = bme.humidity;
+  insidePressure = bme.pressure / 100.0;
+  insideGas = bme.gas_resistance / 1000.0;
+
+  // [FOR DEBUGGING ONLY] Print to serial channel
+  Serial.print(insideTemperature + insideTemperatureUnit + insideHumidity + insideHumidityUnit + insidePressure + insidePressureUnit + insideGas + insideGasUnit);
+  Serial.println();
+  delay(2000);
 }

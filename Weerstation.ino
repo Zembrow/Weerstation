37f359e82efca7727fd524540ff67b5f14114461
Freark de Lange <Frearkdelange2002@hotmail.com>
2023-11-24 23:39:52 +0100

    Added WiFi connection
    - Added basic WiFi connection;
    - Added environment variables;
    - Added .gitignore.

--- Weerstation.ino
+++ Weerstation.ino
@@ -1,8 +1,11 @@
 // Include libraries
+#include <WiFi.h>
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME680.h>
+
+#include "env.h"
 
 // Define components
 Adafruit_BME680 bme; // BME680-Sensor
@@ -19,13 +22,17 @@ float insideGas;
 String insideTemperatureUnit = "*C";
 String insideHumidityUnit = "%";
 String insidePressureUnit = "hPa";
-String insideGasUnit = "KOhms";
+String insideGasUnit = "KOhms"; 
 
 // Setup-function
 // Function runs once when ESP32 is booted
 void setup() {
   // [FOR DEBUGGING ONLY] Open serial channel
   Serial.begin(115200);
+
+  // Start WiFi connection
+  WiFi.begin(WiFiSSID, WiFiPassword);
+  delay(10000);
 
   // Init BME680-sensor
   bme.begin();
@@ -46,7 +53,8 @@ void loop() {
   insideGas = bme.gas_resistance / 1000.0;
 
   // [FOR DEBUGGING ONLY] Print to serial channel
-  Serial.print(insideTemperature + insideTemperatureUnit + insideHumidity + insideHumidityUnit + insidePressure + insidePressureUnit + insideGas + insideGasUnit);
-  Serial.println();
+  //Serial.print(insideTemperature + insideTemperatureUnit + insideHumidity + insideHumidityUnit + insidePressure + insidePressureUnit + insideGas + insideGasUnit);
+  //Serial.println();
+  
   delay(2000);
 }
---
+++ envEmpty.h
@@ -0,0 +1,8 @@
+#ifndef env.h
+#define env.h
+
+// Fill in WiFi SSID & Password
+String WiFiSSID = "";
+String WiFiPassword = "";
+
+#endif
